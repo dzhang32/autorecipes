8af63c604613120d28d86fb2844d338fe3b5b819
David Zhang <dyzhang32@gmail.com>
2021-10-07 13:31:16 +0100

    Refactor and add tests for create_shopping_list()

--- R/shopping_list.R
+++ R/shopping_list.R
@@ -4,23 +4,29 @@
 #'
 #' @return `tibble::tibble()` containing a shopping list.
 #' @export
-#'
-create_shopping_list <- function(recipebook) {
+create_shopping_list <- function(recipebook, method = c("none", "manual", "minimal")) {
     if (!is(recipebook, "RecipeBook")) {
         stop("recipebook must be instance of RecipeBook-class")
     }
     validObject(recipebook)
 
-    ingredients_df <- .extract_ingredients(recipebook)
-    ingredients_collapsed <- .collapse_ingredients(ingredients_df)
+    method <- match.arg(method)
 
-    return(ingredients_collapsed)
+    ingredients_df <- .extract_ingredients(recipebook)
+    shopping_list <- .collapse_ingredients(ingredients_df)
+
+    filter_shopping_list_func <- .dispatch_shopping_list_filterer(method)
+    shopping_list <- filter_shopping_list_func(shopping_list)
+
+    return(shopping_list)
 }
 
 #' @keywords internal
 #' @noRd
 .extract_ingredients <- function(recipebook) {
-    ingredients_df <- recipebook[["ingredients"]] %>%
+    ingredients_df <- recipes(recipebook) %>%
+        dplyr::filter(index %in% meal_plan(recipebook)[["recipe_index"]]) %>%
+        .[["ingredients"]] %>%
         lapply(FUN = as.data.frame) %>%
         do.call(rbind, .)
 
@@ -37,4 +43,97 @@ create_shopping_list <- function(recipebook) {
         dplyr::arrange(dplyr::desc(n))
 
     return(ingredients_collapsed)
+}
+
+#' @keywords internal
+#' @noRd
+.dispatch_shopping_list_filterer <- function(method) {
+    switch(method,
+        "none" = .filter_shopping_list_none,
+        "manual" = .filter_shopping_list_manual,
+        "minimal" = .filter_shopping_list_minimal
+    )
+}
+
+#' @keywords internal
+#' @noRd
+.filter_shopping_list_none <- function(shopping_list) {
+    return(shopping_list)
+}
+
+#' @keywords internal
+#' @noRd
+.filter_shopping_list_manual <- function(shopping_list, con = stdin()) {
+    store_cupboard <- .all_store_cupboard()
+
+    message(
+        stringr::str_c(
+            seq_along(store_cupboard), " - ",
+            store_cupboard, "\n"
+        ),
+        "\nPlease select the store cupboard ingredients you want to include ",
+        "in your shopping list from the above.",
+        "\nEnter the indexes, separated with a ',' - for example '1,2,3' to ",
+        "include the first three ingredients."
+    )
+
+    include_indexes <- readLines(con = con, n = 1L)
+
+    if (include_indexes != "") {
+        include_indexes <- include_indexes %>%
+            stringr::str_split(",") %>%
+            unlist() %>%
+            stringr::str_trim() %>%
+            as.integer()
+
+        store_cupboard <- store_cupboard[-c(include_indexes)]
+    }
+
+    shopping_list <- shopping_list %>%
+        dplyr::filter(!(names %in% store_cupboard))
+
+    return(shopping_list)
+}
+
+#' @keywords internal
+#' @noRd
+.filter_shopping_list_minimal <- function(shopping_list) {
+    store_cupboard <- .all_store_cupboard()
+
+    shopping_list <- shopping_list %>%
+        dplyr::filter(!(names %in% store_cupboard))
+
+    return(shopping_list)
+}
+
+#' @keywords internal
+#' @noRd
+.all_store_cupboard <- function() {
+    store_cupboard <- c(
+        "garlic clove", "garlic",
+        "onion", "red onion",
+        "ginger",
+        "lime", "lemon",
+        "butter", "italian style hard cheese",
+        "egg",
+        "plain flour", "polenta",
+        "bouillon powder", "salt", "pepper", "sugar",
+        "balsamic vinegar",
+        "chilli flakes", "dried chilli flakes",
+        "tomato puree", "worcester sauce",
+        "coarse grain mustard", "dijon mustard",
+        "harissa", "pine nuts",
+        "honey",
+        "dried oregano", "dried rosemary", "dried thyme", "dried mint",
+        "ground ginger", "ground turmeric", "ground cumin",
+        "smoked paprika", "turmeric",
+        "hoisin sauce", "mirin", "sweet chilli sauce", "toasted sesame oil",
+        "tamari",
+        "lasagne spice pot", "masala spice pot", "refried bean spice pot",
+        "seed spice pot", "spiced tomato spice pot", "tostada spice pot",
+        "red pesto", "chipotle paste",
+        "tins chopped tomatoes"
+    )
+
+    return(store_cupboard)
 }
--- tests/testthat/test-shopping_list.R
+++ tests/testthat/test-shopping_list.R
@@ -1,31 +1,85 @@
-# ##### create_shopping_list #####
-#
-# test_that("create_shopping_list output looks correct", {
-#     test_shopping_list <- create_shopping_list(recipebook_example[1:5, ])
-#     expect_false(any(duplicated(test_shopping_list[["names"]])))
-#     expect_true(ncol(test_shopping_list) == 2)
-# })
-#
-# ##### .extract_ingredients #####
-#
-# test_that(".extract_ingredients output looks correct", {
-#     test_ingredients_df <- .extract_ingredients(recipebook_example[1:5, ])
-#     expect_true(is.data.frame(test_ingredients_df))
-# })
-#
-# ##### .collapse_ingredients #####
-#
-# test_that(".collapse_ingredients output looks correct", {
-#     test_ingredients_collapsed <- .collapse_ingredients(dplyr::tibble(
-#         names = stringr::str_c("ingred_", c(1, 1, 2, 3))
-#     ))
-#     expect_true(is.data.frame(test_ingredients_collapsed))
-#     expect_true(identical(
-#         test_ingredients_collapsed[["names"]],
-#         c("ingred_1", "ingred_2", "ingred_3")
-#     ))
-#     expect_true(identical(
-#         test_ingredients_collapsed[["n"]],
-#         c(2L, 1L, 1L)
-#     ))
-# })
+test_RecipeBook <- RecipeBook_example
+meal_plan(test_RecipeBook) <- create_meal_plan(test_RecipeBook)
+
+##### create_shopping_list #####
+
+test_that("create_shopping_list (none) output looks correct", {
+    test_shopping_list <- create_shopping_list(test_RecipeBook)
+    expect_false(any(duplicated(test_shopping_list[["names"]])))
+    expect_true(ncol(test_shopping_list) == 2)
+    expect_true(any(.all_store_cupboard() %in% test_shopping_list[["names"]]))
+})
+
+test_that("create_shopping_list (minimal) output looks correct", {
+    test_shopping_list <- create_shopping_list(test_RecipeBook, "minimal")
+    expect_false(any(.all_store_cupboard() %in% test_shopping_list[["names"]]))
+})
+
+test_that("create_shopping_list catches user-input errors", {
+    expect_error(
+        create_shopping_list(RecipeBook_example, method = "not_a_method"),
+        "should be one of"
+    )
+    expect_error(
+        create_shopping_list(tibble::tibble()),
+        "recipebook must be instance of RecipeBook-class"
+    )
+    expect_error(
+        create_shopping_list(RecipeBook_example),
+        "No meal plan found, have you run create_meal_plan"
+    )
+})
+
+##### .filter_shopping_list_manual #####
+
+test_that(".filter_shopping_list_manual output looks correct", {
+    test_shopping_list <- create_shopping_list(test_RecipeBook, method = "none")
+    test_sc <- .all_store_cupboard()
+    # setup testing to mirror manual input
+    # based on https://debruine.github.io/posts/interactive-test/
+    input <- file()
+    lines <- "1,2,3"
+    write(lines, input)
+    test_manual <- .filter_shopping_list_manual(
+        test_shopping_list,
+        con = input
+    )
+    close(input)
+
+    expect_true(nrow(test_manual) < nrow(test_shopping_list))
+    expect_false(any(test_sc[4:length(test_sc)] %in% test_manual[["names"]]))
+})
+
+##### .extract_ingredients #####
+
+test_that(".extract_ingredients output looks correct", {
+    recipes_test <- recipes(test_RecipeBook) %>%
+        .[meal_plan(test_RecipeBook)[["recipe_index"]], ]
+    ingredients_test <- recipes_test[["ingredients"]] %>%
+        lapply(function(x) x@names) %>%
+        unlist() %>%
+        sort()
+
+    test_ingredients_df <- .extract_ingredients(test_RecipeBook)
+    expect_true(identical(
+        sort(test_ingredients_df[["names"]]),
+        ingredients_test
+    ))
+})
+
+##### .collapse_ingredients #####
+
+test_that(".collapse_ingredients output looks correct", {
+    test_ingredients_collapsed <- .collapse_ingredients(dplyr::tibble(
+        names = stringr::str_c("ingred_", c(1, 1, 2, 3))
+    ))
+    expect_true(is.data.frame(test_ingredients_collapsed))
+    expect_true(identical(
+        test_ingredients_collapsed[["names"]],
+        c("ingred_1", "ingred_2", "ingred_3")
+    ))
+    expect_true(identical(
+        test_ingredients_collapsed[["n"]],
+        c(2L, 1L, 1L)
+    ))
+})
