73a56caa6d892048f9c027c871be407c0b348c11
David Zhang <dyzhang32@gmail.com>
2021-10-16 20:57:57 +0100

    feat(rm_recipes): add function to remove recipes

--- R/set_recipes.R
+++ R/set_recipes.R
@@ -3,6 +3,7 @@
 #' Add or remove recipes
 #'
 #' @inheritParams RecipeBook-class
+#' @inheritParams set_favourites
 #' @param recipebook object of `RecipeBook-class`.
 #'
 #' @name set_recipes
@@ -24,6 +25,83 @@ add_recipes <- function(recipebook,
     dplyr::mutate(index = dplyr::row_number())
 
   validObject(recipebook)
+
+  return(recipebook)
+}
+
+#' @rdname set_recipes
+#' @export
+rm_recipes <- function(recipebook,
+                       indexes = NULL,
+                       method = c("manual", "index")) {
+  .check_object(recipebook, "RecipeBook")
+  method <- match.arg(method)
+
+  recipes_remover_func <- .dispatch_recipe_remover(method)
+  indexes <- recipes_remover_func(recipebook, indexes)
+
+  recipes(recipebook) <- recipes(recipebook) %>%
+    dplyr::filter(!(index %in% indexes)) %>%
+    dplyr::mutate(index = dplyr::row_number())
+
+  recipebook <- .reset_meal_plan(recipebook)
+
+  return(recipebook)
+}
+
+#' @keywords internal
+#' @noRd
+.dispatch_recipe_remover <- function(method) {
+  switch(method,
+    "index" = .rm_recipes_index,
+    "manual" = .rm_recipes_manual
+  )
+}
+
+#' @keywords internal
+#' @noRd
+.rm_recipes_index <- function(recipebook, indexes) {
+  if (is.null(indexes)) {
+    stop("'index' method requires non-NULL indexes")
+  } else if (!is.numeric(indexes)) {
+    stop("indexes must be numeric")
+  }
+  indexes <- as.integer(indexes)
+
+  return(indexes)
+}
+
+#' @keywords internal
+#' @noRd
+.rm_recipes_manual <- function(recipebook, indexes, con = stdin()) {
+  message(
+    stringr::str_c(
+      seq_along(names(recipebook)), " - ",
+      names(recipebook), "\n"
+    ),
+    "\nPlease select the recipes to remove from the above.",
+    "\nEnter the indexes, separated with a ',' - for example '1,2,3' to ",
+    "favourite the first three recipes."
+  )
+
+  indexes <- readLines(con = con, n = 1L)
+
+  indexes <- indexes %>%
+    stringr::str_split(",") %>%
+    unlist() %>%
+    stringr::str_trim() %>%
+    as.integer()
+
+  return(indexes)
+}
+
+#' @keywords internal
+#' @noRd
+.reset_meal_plan <- function(recipebook) {
+  if (nrow(recipebook@meal_plan) > 0) {
+    message("Resetting meal plan, as indexes of recipes have been changed")
+    meal_plan(recipebook) <- dplyr::tibble()
+  }
 
   return(recipebook)
 }
--- tests/testthat/test-set_recipes.R
+++ tests/testthat/test-set_recipes.R
@@ -16,3 +16,59 @@ test_that("add_recipes output looks correct", {
     test_recipebook
   ))
 })
+
+##### rm_recipes #####
+
+test_that("rm_recipes (index) output looks correct", {
+  test_recipebook <- create_meal_plan(test_recipebook)
+  test_recipebook_rmed <- rm_recipes(test_recipebook, 1:10, "index")
+  expect_true(length(test_recipebook_rmed) == length(test_recipebook) - 10)
+  expect_true(identical(
+    recipes(test_recipebook_rmed)[["names"]],
+    recipes(test_recipebook)[["names"]][-c(1:10)]
+  ))
+  expect_true(identical(
+    recipes(test_recipebook_rmed)[["index"]],
+    seq_along(recipes(test_recipebook_rmed)[["index"]])
+  ))
+  expect_true(identical(
+    dplyr::tibble(),
+    test_recipebook_rmed@meal_plan
+  ))
+})
+
+test_that("rm_recipes (index) output looks correct", {
+  test_recipebook <- create_meal_plan(test_recipebook)
+  test_recipebook_rmed <- rm_recipes(test_recipebook, 1:10, "index")
+  expect_true(length(test_recipebook_rmed) == length(test_recipebook) - 10)
+  expect_true(identical(
+    recipes(test_recipebook_rmed)[["names"]],
+    recipes(test_recipebook)[["names"]][-c(1:10)]
+  ))
+  expect_true(identical(
+    recipes(test_recipebook_rmed)[["index"]],
+    seq_along(recipes(test_recipebook_rmed)[["index"]])
+  ))
+  expect_true(identical(
+    dplyr::tibble(),
+    test_recipebook_rmed@meal_plan
+  ))
+})
+
+test_that("rm_recipes (manual) output looks correct", {
+  # setup testing to mirror manual input
+  # based on https://debruine.github.io/posts/interactive-test/
+  input <- file()
+  lines <- "1,2,3"
+  write(lines, input)
+  test_manual <- .rm_recipes_manual(
+    test_recipebook,
+    con = input
+  )
+  close(input)
+
+  expect_true(identical(
+    test_manual,
+    1:3
+  ))
+})
